7598ce81d1ea606f5f0504f3a93f831d0108df66
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-03 17:35:48 +0300

    modified:   Maman11/QuestionOne/src/MyProject.java

--- Maman11/QuestionOne/src/MyProject.java
+++ Maman11/QuestionOne/src/MyProject.java
@@ -1,4 +1,13 @@
 
 public class MyProject {
+    //what I need to do
+    /*
+     * Use arrayList
+     * Class Pile:
+     * Number, Colour, Shape
+     * Contructors, getters, equals, toString
+     * Class Card
+     * Controller/Main Class
+     */
 
 }
