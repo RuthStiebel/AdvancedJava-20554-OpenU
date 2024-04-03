8728aee75b3a41070c114f92452c8f842fccba05
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-03 17:40:14 +0300

    added work outline:QuestionOne/src/MyProject.java

--- Maman11/QuestionOne/src/MyProject.java
+++ Maman11/QuestionOne/src/MyProject.java
@@ -3,11 +3,16 @@ public class MyProject {
     //what I need to do
     /*
      * Use arrayList
-     * Class Pile:
+     * Class Card:
      * Number, Colour, Shape
      * Contructors, getters, equals, toString
-     * Class Card
+     * Class Pile
+     * ArrayList from type of card
+     * Contructor, Shuffler, isEmpty, NumOfCardsLeft, Dealer, removing card (top), adding card (bottom), remove three, 
      * Controller/Main Class
+     * Graphics class^ - sends to user notes blahblahblah
+     * Logic class
+     * initializes pile, shuffles and etc
      */
 
 }
