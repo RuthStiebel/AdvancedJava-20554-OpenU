cf54aaf6a27c45b4c7ff48a5b94e135235424c7d
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-04 09:31:08 +0300

    new file:   Maman11/QuestionOne/src/Card.java 	new file:   Maman11/QuestionOne/src/CardPile.java 	deleted:    Maman11/QuestionOne/src/MyProject.java 	deleted:    Maman11/QuestionOne/src/MyProjectController.java

---
+++ Maman11/QuestionOne/src/Card.java
@@ -0,0 +1,30 @@
+
+public class Card {
+    private String shape;
+    private String colour;
+    private int value;
+
+    public String getShape () {
+
+    }
+    public String getColour () {
+
+    }
+    public int getValue () {
+        
+    }
+    public boolean equals (Card card) {
+
+    }
+    public String toString () {
+
+    }
+
+    //what I need to do
+    /*
+     * Use arrayList
+     * Class Card:
+     * Number, Colour, Shape
+     * Contructors, getters, equals, toString
+     */
+}
---
+++ Maman11/QuestionOne/src/CardPile.java
@@ -0,0 +1,15 @@
+
+public class CardPile {
+
+}
+/*
+Class Pile
+* ArrayList from type of card
+* Contructor, Shuffler, isEmpty, NumOfCardsLeft, Dealer, removing card (top), adding card (bottom), remove three, 
+* Controller/Main Class
+* Graphics class^ - sends to user notes blahblahblah
+* Logic class
+* initializes pile, shuffles and etc
+* 
+* close game with  System.exit
+*/
