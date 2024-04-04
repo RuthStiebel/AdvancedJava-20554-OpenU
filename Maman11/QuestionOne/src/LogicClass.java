ba435ec7857d21925734fbc17fbf54b31831bacb
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-04 14:08:42 +0300

    modified:   Maman11/QuestionOne/src/LogicClass.java

--- Maman11/QuestionOne/src/LogicClass.java
+++ Maman11/QuestionOne/src/LogicClass.java
@@ -1,4 +1,9 @@
 public class LogicClass {
+    //check the classes in book
+    //need to initialize two piles
+    //need to start game (divide the deck in half and shuffle)
+    //need to prompt the prnting on the screen
+    //need to end game
 /*
  * * Logic class
 * initializes pile, shuffles and etc
