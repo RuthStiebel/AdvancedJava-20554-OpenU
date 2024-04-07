b019592d5b5d4e8627973d25bdc7444bdec98212
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-07 14:27:18 +0300

    added functions:   Maman11/QuestionOne/src/LogicClass.java

--- Maman11/QuestionOne/src/LogicClass.java
+++ Maman11/QuestionOne/src/LogicClass.java
@@ -1,5 +1,5 @@
 public class LogicClass {
-    
+
     // divide the cards from deck MainDeck to dackA and deckB
     public static void dealDeck(DeckOfCards deckA, DeckOfCards deckB, DeckOfCards MainDeck) {
         for (int i = 0; i < 52; i++) {
@@ -10,9 +10,47 @@ public class LogicClass {
         }
     }
 
+    public static void compareCardsAndUpdateDecks(DeckOfCards firstPlayer, DeckOfCards secondPlayer,
+            DeckOfCards helperDeck, Card firstCard, Card secondCard) {
+        if (firstCard.compare(secondCard) == 1) { // this means that the first card is bigger
+            firstPlayer.mergeDecks(helperDeck);
+        } else if (firstCard.compare(secondCard) == -1) {
+            secondPlayer.mergeDecks(helperDeck);
+        }
+    }
+
+    /**
+     * 
+     * @param firstPlayer
+     * @param secondPlayer
+     * @param helperDeck
+     * @param firstCard
+     * @param secondCard
+     * @return True if both decks are not empty
+     */
+    public static boolean warTime (DeckOfCards firstPlayer, DeckOfCards secondPlayer,
+    DeckOfCards helperDeck, Card firstCard, Card secondCard) {
+        final int WAR_TIME = 3;
+        boolean flag = true;
+        for (int i = 1; i < WAR_TIME; i++) { //remove the first two 
+            if (!helperDeck.addCard(firstPlayer.dealCard()) || !helperDeck.addCard(secondPlayer.dealCard())) {
+                flag = false;
+                break;
+            }
+        }
+        firstCard = firstPlayer.dealCard();
+        secondCard = secondPlayer.dealCard();
+        helperDeck.addCard(firstCard);
+        helperDeck.addCard(secondCard);
+        if (firstCard.compare(secondCard) == 1 || firstCard.compare(secondCard) == -1) { // this means that one of the cards is bigger
+            compareCardsAndUpdateDecks (firstPlayer, secondPlayer, helperDeck, firstCard, secondCard);
+        } else { //again war
+            flag = warTime(firstPlayer, secondPlayer, helperDeck, null, null);
+        }
+        return flag;
+    }
     public static void main(String[] args) {
         final int HALF_A_DECK = 26;
-        final int WAR_TIME = 3;
         boolean flag = true;
         DeckOfCards deckOfCards = new DeckOfCards();
         deckOfCards.shuffle(); //shuffles
@@ -29,21 +67,11 @@ public class LogicClass {
             secondCard = secondPlayer.dealCard();
             helperDeck.addCard(firstCard);
             helperDeck.addCard(secondCard);
-            if (firstCard.compare(secondCard) == 1) { // this means that the first card is bigger
-                firstPlayer.mergeDecks(helperDeck);
-            } else if (firstCard.compare(secondCard) == -1) {
-                secondPlayer.mergeDecks(helperDeck);
+            if (firstCard.compare(secondCard) == 1 || firstCard.compare(secondCard) == -1) { // this means that one of the cards is bigger
+                compareCardsAndUpdateDecks (firstPlayer, secondPlayer, helperDeck, firstCard, secondCard);
             } else { //means they are equal
-                for (int i = 1; i < WAR_TIME; i++) { //remove the first two 
-                    if (!helperDeck.addCard(firstPlayer.dealCard()) || !helperDeck.addCard(secondPlayer.dealCard())) {
-                        flag = false;
-                        break;
-                    }
-                }
-
+                flag = warTime(firstPlayer, secondPlayer, helperDeck, null, null);
             }
-
-        }
         //need a while that goes on until one of them is empty
         //then I need to have each one deal a caard and compare them
         //if the first one wins the put both cards in the first deck
@@ -56,5 +84,6 @@ public class LogicClass {
         // need to prompt the prnting on the screen
         // need to end game
 
+        }
     }
 }
