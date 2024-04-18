d62ce12b44d2167c9b1d1ac0f4c690d7e068e9c2
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-18 14:45:44 +0300

    modified:   Maman12/QuestionOne/src/PayrollSystemTest.java

--- Maman12/QuestionOne/src/PayrollSystemTest.java
+++ Maman12/QuestionOne/src/PayrollSystemTest.java
@@ -2,13 +2,13 @@ public class PayrollSystemTest {
    public static void main(String[] args) {
       // create subclass objects                                          
       SalariedEmployee salariedEmployee =                                 
-         new SalariedEmployee("John", "Smith", "111-11-1111", "11.12.1994", 800.00);    
+         new SalariedEmployee("John", "Smith", "111-11-1111", "11.3.1994", 800.00);    
       HourlyEmployee hourlyEmployee =                                     
          new HourlyEmployee("Karen", "Price", "222-22-2222", "29.1.1990", 16.75, 40);  
       CommissionEmployee commissionEmployee =                             
          new CommissionEmployee("Sue", "Jones", "333-33-3333", "31.6.1885", 10000, .06);                      
       BasePlusCommissionEmployee basePlusCommissionEmployee =             
-         new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", "10.10.1910", 5000, .04, 300);                  
+         new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", "10.4.1910", 5000, .04, 300);                  
  
         System.out.println("Employees processed individually:");
 
@@ -50,6 +50,10 @@ public class PayrollSystemTest {
                "new base salary with 10%% increase is: $%,.2f%n",
                employee.getBaseSalary());
          } 
+         if (200==currentEmployee.isBirthMonth(4)) {
+            System.out.printf(
+            "received birthday bonus of $200%n");
+         }
          System.out.printf(
             "earned $%,.2f%n%n", currentEmployee.earnings()+currentEmployee.isBirthMonth(4));
       } 
