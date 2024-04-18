632a0850731bfae14928b67cb5c87e5c99d73ecf
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-18 15:46:29 +0300

    modified:   Maman12/QuestionOne/src/DateOfBirth.java 	modified:   Maman12/QuestionOne/src/PieceWorker.java

--- Maman12/QuestionOne/src/DateOfBirth.java
+++ Maman12/QuestionOne/src/DateOfBirth.java
@@ -1,26 +1,68 @@
 public class DateOfBirth {
-    private int day;
-    private int month;
-    private int year;
+    private int month; // 1-12
+    private int day; // 1-31 based on month
+    private int year; // any year
+    private final int LEAP_YEAR_MONTH = 2;
+    private final int LEAP_YEAR_DAY = 29;
+    private static final int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-    public DateOfBirth(int day, int month, int year) {
-        this.day = day;
+    public DateOfBirth(int month, int day, int year) {
+        if (!isValidDate(month, day, year)) {
+            throw new IllegalArgumentException("Invalid date");
+        }
+
         this.month = month;
+        this.day = day;
         this.year = year;
+
+        System.out.printf("Date object constructor for date %s%n", this);
     }
 
-    // this constructor turns a string into a DateOfBirth object
     public DateOfBirth(String date) {
-        this(0, 0, 0);
         String[] dates = date.split("\\.");
         if (dates.length != 3) {
-            throw new IllegalArgumentException("Wrong number of arguments in date.");
+            throw new IllegalArgumentException("Wrong number of arguments in date");
         } else {
-            this.day = Integer.parseInt(dates[0]);
-            this.month = Integer.parseInt(dates[1]);
-            this.year = Integer.parseInt(dates[2]);
+            int month = Integer.parseInt(dates[0]);
+            int day = Integer.parseInt(dates[1]);
+            int year = Integer.parseInt(dates[2]);
+            if (!isValidDate(month, day, year)) {
+                throw new IllegalArgumentException("Invalid date");
+            } else {
+                this.month = month;
+                this.day = day;
+                this.year = year;
+
+                System.out.printf("Date object constructor for date %s%n", this);
+            }
         }
-    } 
+    }
+
+    private boolean isValidDate(int month, int day, int year) {
+        // check if month in range
+        if (month <= 0 || month > 12) {
+            throw new IllegalArgumentException(
+                    "month (" + month + ") must be 1-12");
+        }
+
+        // check if day in range for month
+        if (day <= 0 ||
+                (day > daysPerMonth[month] && !(month == 2 && day == 29))) {
+            throw new IllegalArgumentException("day (" + day +
+                    ") out-of-range for the specified month and year");
+        }
+
+        // check for leap year if month is 2 and day is 29
+        if (month == LEAP_YEAR_MONTH && day == LEAP_YEAR_DAY && !isLeapYear(year)) {
+            throw new IllegalArgumentException("day (" + day +
+                    ") out-of-range for the specified month and year");
+        }
+        return true;
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
 
     // getters
     public int getDay() {
@@ -35,7 +77,8 @@ public class DateOfBirth {
         return year;
     }
 
+    // return a String of the form month/day/year
     public String toString() {
-        return (day + "." + month + "." + year);
+        return String.format("%d/%d/%d", month, day, year);
     }
 }
--- Maman12/QuestionOne/src/PieceWorker.java
+++ Maman12/QuestionOne/src/PieceWorker.java
@@ -1,67 +1,64 @@
 public class PieceWorker extends Employee {
     private double wage; // wage per hour
     private int pieces; // hours worked for week
- 
+
     // constructor
     public PieceWorker(String firstName, String lastName,
-       String socialSecurityNumber, String dateOfBirth, double wage, int pieces) {
-       super(firstName, lastName, socialSecurityNumber, dateOfBirth);
- 
-       if (wage < 0.0) { // validate wage
-          throw new IllegalArgumentException("Hourly wage must be >= 0.0");
-       }
- 
-       if (pieces < 0.0) { // validate pieces
-          throw new IllegalArgumentException(
-             "Pieces made must be >= 0.0");
-       }
- 
-       this.wage = wage;
-       this.pieces = pieces;
-    } 
- 
+            String socialSecurityNumber, String dateOfBirth, double wage, int pieces) {
+        super(firstName, lastName, socialSecurityNumber, dateOfBirth);
+
+        if (wage < 0.0) { // validate wage
+            throw new IllegalArgumentException("Hourly wage must be >= 0.0");
+        }
+
+        if (pieces < 0.0) { // validate pieces
+            throw new IllegalArgumentException(
+                    "Pieces made must be >= 0.0");
+        }
+
+        this.wage = wage;
+        this.pieces = pieces;
+    }
+
     // set wage
     public void setWage(double wage) {
-       if (wage < 0.0) { // validate wage
-          throw new IllegalArgumentException("Hourly wage must be >= 0.0");
-       }
- 
-       this.wage = wage;
-    } 
- 
+        if (wage < 0.0) { // validate wage
+            throw new IllegalArgumentException("Piece wage must be >= 0.0");
+        }
+
+        this.wage = wage;
+    }
+
     // return wage
-    public double getWage() {return wage;}
- 
+    public double getWage() {
+        return wage;
+    }
+
     // set hours worked
     public void setPieces(int pieces) {
-       if ((pieces < 0.0) || (pieces > 168.0)) { // validate hours
-          throw new IllegalArgumentException(
-             "Hours worked must be >= 0.0 and <= 168.0");
-       }
- 
-       this.pieces = pieces;
-    } 
- 
+        if (pieces < 0.0) { // validate pieces
+            throw new IllegalArgumentException(
+                    "Pieces made must be >= 0.0");
+        }
+        this.pieces = pieces;
+    }
+
     // return pieces made
-    public int getPieces() {return pieces;}
- 
+    public int getPieces() {
+        return pieces;
+    }
+
     // calculate earnings; override abstract method earnings in Employee
-    @Override                                                           
-    public double earnings() {                                          
-       if (getPieces() <= 40) { // no overtime                           
-          return getWage() * getPieces();                                
-       }                                                                
-       else {                                                           
-          return 40 * getWage() + (getPieces() - 40) * getWage() * 1.5;  
-       }                                                                
-    }                                                                   
- 
-    // return String representation of HourlyEmployee object              
-    @Override                                                             
-    public String toString() {                                            
-       return String.format("hourly employee: %s%n%s: $%,.2f; %s: %,.2f", 
-          super.toString(), "hourly wage", getWage(),                     
-          "hours worked", getPieces());                                    
-    }                                                                     
- } 
- 
+    @Override
+    public double earnings() {
+        return getWage() * getPieces();
+    }
+
+    // return String representation of HourlyEmployee object
+    @Override
+    public String toString() {
+        return String.format("piece worker: %s%n%s: $%,.2f; %s: %,.2f",
+                super.toString(), "piece wage", getWage(),
+                "pieces made", getPieces());
+    }
+}
