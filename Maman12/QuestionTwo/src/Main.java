49d3ba9a6bf09d1d825a40ff66276fbe62f7a8d2
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-21 21:11:53 +0300

    modified:   Maman12/QuestionTwo/bin/Main.class 	modified:   Maman12/QuestionTwo/src/Main.java

--- Maman12/QuestionTwo/src/Main.java
+++ Maman12/QuestionTwo/src/Main.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        
+        System.out.println(
+                "Enter a rational number, when the numerator and denminator are seperated by a //, "
+                        + "the numerator is a whole number and the denomantor is a positive number.");
+        String rationalNum = scan.nextLine();
+
+        scan.close();
     }
 
 }
