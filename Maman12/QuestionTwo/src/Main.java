7a3008019817249ec89c8e1454d2f7a28fd5a474
Ruth Stiebel <stiebelruth@gmail.com>
2024-04-22 14:54:55 +0300

    modified:   Maman12/QuestionTwo/bin/Main.class 	modified:   Maman12/QuestionTwo/src/Main.java

--- Maman12/QuestionTwo/src/Main.java
+++ Maman12/QuestionTwo/src/Main.java
@@ -8,11 +8,18 @@ public class Main {
                 "Enter a rational number, when the numerator and denminator are seperated by a //, "
                         + "the numerator is a whole number and the denomantor is a positive number.");
         String rationalNum = scan.nextLine();
+
+        try {
+            rationalNum.split("/");
+        }
+        catch (Exception e) {
+             
+        }
         //check if there is a slash
         //check if there are only two numbers - one on either side of the slash
         //send to Rational constructor
         //check if Rational number was created
-        
+
         scan.close();
     }
 
