311bcf6e651bb42cb4551487d565cd4501ccd983
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-01 15:24:04 +0300

    modified:   Maman13/QuestionOne/bin/Paint.fxml 	modified:   Maman13/QuestionOne/bin/PaintController.class 	modified:   Maman13/QuestionOne/src/PaintController.java

--- Maman13/QuestionOne/src/PaintController.java
+++ Maman13/QuestionOne/src/PaintController.java
@@ -1,12 +1,20 @@
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 public class PaintController {
 
     @FXML
     private Canvas canv;
 
+    @FXML
+    private GridPane grid;
+
+
+    GraphicsContext gc = canv.getGraphicsContext2D();
+    
     @FXML
     void mouseClicked(MouseEvent event) {
 
