709610608f0b77ec9a14019152dde7d2e2aafe72
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-01 15:12:33 +0300

    new file:   Maman13/QuestionOne/bin/PaintController.class 	new file:   Maman13/QuestionOne/src/PaintController.java

---
+++ Maman13/QuestionOne/src/PaintController.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+
+public class PaintController {
+
+    @FXML
+    private Canvas canv;
+
+    @FXML
+    void mouseClicked(MouseEvent event) {
+
+    }
+
+}
