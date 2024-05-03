dbd27bc2e5b15768f08d3bd89bc0ae0bf36185db
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-03 19:21:41 +0300

    modified:   Maman13/QuestionOne/bin/PaintController.class 	modified:   Maman13/QuestionOne/src/PaintController.java

--- Maman13/QuestionOne/src/PaintController.java
+++ Maman13/QuestionOne/src/PaintController.java
@@ -1,6 +1,7 @@
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
@@ -14,7 +15,12 @@ public class PaintController {
 
 
     GraphicsContext gc = canv.getGraphicsContext2D();
-    
+    private Button[] buttons;
+    private final int NUM_OF_SHAPES = 8;
+
+    private void createButtons () {
+        
+    }
     @FXML
     void mouseClicked(MouseEvent event) {
 
