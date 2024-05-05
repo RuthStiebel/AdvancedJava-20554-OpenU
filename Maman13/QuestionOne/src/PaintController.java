987deb4ed7829adea1634b767eb0b5a5260b19e1
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-05 23:03:03 +0300

    new file:   Maman13/QuestionOne/bin/PaintController$1.class 	modified:   Maman13/QuestionOne/bin/PaintController.class 	modified:   Maman13/QuestionOne/src/PaintController.java

--- Maman13/QuestionOne/src/PaintController.java
+++ Maman13/QuestionOne/src/PaintController.java
@@ -1,3 +1,5 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -13,16 +15,31 @@ public class PaintController {
     @FXML
     private GridPane grid;
 
-
     GraphicsContext gc = canv.getGraphicsContext2D();
     private Button[] buttons;
     private final int NUM_OF_SHAPES = 8;
     private final int SIZE = 5;
 
-    private void createButtons () {
-        buttons = new Button[SIZE*SIZE];
-
+    private void createButtons() {
+        buttons = new Button[SIZE * SIZE];
+        for (int i = 0; i < SIZE * SIZE; i++) {
+            buttons[i] = new Button();
+            buttons[i].setPrefSize(grid.getPrefWidth() / SIZE, grid.getPrefHeight() / SIZE);
+            buttons[i].setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    handleButton(e);
+                }
+            });
+            grid.add(buttons[i], i % SIZE, i / SIZE);
+        }
     }
+    
+    private void handleButton(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleButton'");
+    }
+
     @FXML
     void mouseClicked(MouseEvent event) {
 
