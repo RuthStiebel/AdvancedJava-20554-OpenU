6f2c551444be85104eab7276fe7f79801c9a0250
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-07 15:11:35 +0300

    modified:   Maman13/QuestionOne/src/PaintController.java

--- Maman13/QuestionOne/src/PaintController.java
+++ Maman13/QuestionOne/src/PaintController.java
@@ -25,35 +25,3 @@ public class PaintController {
     }
 
 }
-/*
- *   @FXML
-    private Canvas canv;
-
-    @FXML
-    private GridPane grid;
-
-    GraphicsContext gc = canv.getGraphicsContext2D();
-    private Button[] buttons;
-    private final int NUM_OF_SHAPES = 8;
-    private final int SIZE = 5;
-
-    private void createButtons() {
-        buttons = new Button[SIZE * SIZE];
-        for (int i = 0; i < SIZE * SIZE; i++) {
-            buttons[i] = new Button();
-            buttons[i].setPrefSize(grid.getPrefWidth() / SIZE, grid.getPrefHeight() / SIZE);
-            buttons[i].setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent e) {
-                    handleButton(e);
-                }
-            });
-            grid.add(buttons[i], i % SIZE, i / SIZE);
-        }
-    }
-
-    private void handleButton(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleButton'");
-
- */
