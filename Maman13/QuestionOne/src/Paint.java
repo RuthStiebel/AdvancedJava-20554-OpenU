faf073dfdf5dbdc7de3cafa56e67627950e7af81
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-07 15:12:46 +0300

    modified:   Maman13/QuestionOne/bin/Paint.class 	modified:   Maman13/QuestionOne/src/Paint.java

--- Maman13/QuestionOne/src/Paint.java
+++ Maman13/QuestionOne/src/Paint.java
@@ -1,3 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
 public class Paint {
+     /**
+       * The main method to launch the application.
+       */
+      public static void main(String[] args) {
+            launch(args);
+            System.out.println();
+      }
+
+      /**
+       * Starts the JavaFX application by loading the FXML file and setting up the
+       * stage.
+       * 
+       * @param stage The primary stage of the application.
+       * @throws Exception If an error occurs while loading the FXML file.
+       */
+      public void start(Stage stage) throws Exception {
+            Parent root = (Parent) FXMLLoader.load(getClass().getResource("PayrollSystemTest.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("PayrollSystemTest");
+            stage.setScene(scene);
+            stage.show();
+      }
+
 
 }
