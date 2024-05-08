bfc885519fdcb0a9caef26e6397e716d0affe828
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-08 17:38:46 +0300

    modified:   Maman13/QuestionTwo/bin/Logic.class 	modified:   Maman13/QuestionTwo/src/Logic.java

--- Maman13/QuestionTwo/src/Logic.java
+++ Maman13/QuestionTwo/src/Logic.java
@@ -1,5 +1,32 @@
-public class Logic {
-    //here we check whos turn is it, did they win and if the board is full
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Logic extends Application {
+    /**
+     * The main method to launch the application.
+     */
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println();
+    }
+
+    /**
+     * Starts the JavaFX application by loading the FXML file and setting up the
+     * stage.
+     * 
+     * @param stage The primary stage of the application.
+     * @throws Exception If an error occurs while loading the FXML file.
+     */
+    public void start(Stage stage) throws Exception {
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("Paint.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Paint");
+        stage.setScene(scene);
+        stage.show();
+    }
+    // here we check whos turn is it, did they win and if the board is full
 
 }
