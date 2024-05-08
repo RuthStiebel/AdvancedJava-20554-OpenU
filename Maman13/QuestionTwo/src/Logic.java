bd40ebb871391b6132168fc832640a8fa518c423
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-08 17:37:44 +0300

    new file:   Maman13/QuestionTwo/bin/Logic.fxml 	new file:   Maman13/QuestionTwo/bin/LogicController.class 	modified:   Maman13/QuestionTwo/src/Logic.java 	new file:   Maman13/QuestionTwo/src/LogicController.java

--- Maman13/QuestionTwo/src/Logic.java
+++ Maman13/QuestionTwo/src/Logic.java
@@ -1,3 +1,5 @@
 public class Logic {
+    //here we check whos turn is it, did they win and if the board is full
+    
 
 }
---
+++ Maman13/QuestionTwo/src/LogicController.java
@@ -0,0 +1,31 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class LogicController {
+
+    @FXML
+    private GridPane grid;
+    private final int SIZE = 7;
+
+    private Button buttons[];
+
+    public void initialize() {
+        buttons = new Button[SIZE * SIZE];
+
+        for (int i = 0; i < SIZE * (SIZE - 1); i++) {
+            if (i < 42 || i == 46) {
+                if (i == 46) // the center of the bottom line is the clear button
+                    buttons[i] = new Button("Clear");
+                buttons[i] = new Button();
+                buttons[i].setPrefSize(grid.getPrefWidth() / SIZE,
+                        grid.getPrefHeight() / SIZE);
+                grid.add(buttons[i], i % SIZE, i / SIZE);
+            }
+        }
+
+    }
+
+}
+
+// here we build the board, colour the circles and print messages to the user
