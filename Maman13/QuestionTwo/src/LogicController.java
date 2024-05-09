91182375d8b54e71f007edaabd25370dd31375be
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-09 14:11:29 +0300

    modified:   Maman13/QuestionTwo/bin/Logic.fxml 	new file:   Maman13/QuestionTwo/bin/LogicController$1.class 	modified:   Maman13/QuestionTwo/bin/LogicController.class 	modified:   Maman13/QuestionTwo/src/LogicController.java

--- Maman13/QuestionTwo/src/LogicController.java
+++ Maman13/QuestionTwo/src/LogicController.java
@@ -3,15 +3,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+
 
 public class LogicController {
 
     @FXML
     private GridPane topGrid;
-
-    @FXML
-    private GridPane bottomGrid;
 
     private final int SIZE = 7;
 
@@ -21,41 +18,33 @@ public class LogicController {
         buttons = new Button[SIZE * SIZE];
 
         for (int i = 0; i < SIZE * SIZE; i++) {
-            if (i < 35) {
-                if (i == 45) // the center of the bottom line is the clear button
-                {
-                    buttons[i] = new Button("Clear");
-                    addButtonToGrid(bottomGrid, buttons[i], i);
-                } else {
-
+            if (i < 35 ) {
                     buttons[i] = new Button();
                     addButtonToGrid(topGrid, buttons[i], i);
-                }
 
-                /*
-                 * buttons[i].setOnAction(new EventHandler<ActionEvent>() {
-                 * 
-                 * @Override
-                 * public void handle (ActionEvent event) {
-                 * 
-                 * }
-                 * });
-                 */
+                
+                 buttons[i].setOnAction(new EventHandler<ActionEvent>() {
+                 
+                 @Override
+                 public void handle (ActionEvent event) {
+                 
+                 }
+                 });
+                
             }
-            if (i > 35 && i < 43) {
-                int number = i - 35; // Numbers 1 to 7
-                Text text = new Text(Integer.toString(number));
-                text.setStyle("-fx-font-size: 20;"); // Set font size
-                buttonGrid.add(text, (i - 36) % SIZE, SIZE);
+            //TODO need to add numbers under the columns
+            if (i == 45) // the center of the bottom line is the clear button
+            {
+                buttons[i] = new Button("Clear");
+                addButtonToGrid(topGrid, buttons[i], i);
             }
-
         }
     }
 
     private void addButtonToGrid(GridPane grid, Button btn, int place) {
-        btn.setPrefSize(topGrid.getPrefWidth() / SIZE,
-                topGrid.getPrefHeight() / SIZE);
-        topGrid.add(btn, place % SIZE, place / SIZE);
+        btn.setPrefSize(grid.getPrefWidth() / SIZE,
+                grid.getPrefHeight() / SIZE);
+        grid.add(btn, place % SIZE, place / SIZE);
     }
 }
 
