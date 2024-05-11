36acec791505d4890366d16b4723602541de8ccb
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-11 11:42:48 +0300

    modified:   Maman13/QuestionTwo/bin/LogicController$1.class 	modified:   Maman13/QuestionTwo/bin/LogicController.class 	modified:   Maman13/QuestionTwo/src/LogicController.java

--- Maman13/QuestionTwo/src/LogicController.java
+++ Maman13/QuestionTwo/src/LogicController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
