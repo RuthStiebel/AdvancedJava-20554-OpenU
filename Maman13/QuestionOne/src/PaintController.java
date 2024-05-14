2c9689fc05a0cea62e8eee3634c3679a95d982da
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-14 12:35:51 +0300

    modified:   Maman13/QuestionOne/bin/Paint.fxml 	modified:   Maman13/QuestionOne/bin/PaintController.class 	modified:   Maman13/QuestionOne/src/PaintController.java

--- Maman13/QuestionOne/src/PaintController.java
+++ Maman13/QuestionOne/src/PaintController.java
@@ -2,8 +2,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class PaintController {
+
+    @FXML
+    private MenuItem circle;
 
     @FXML
     private Button clearButton;
@@ -15,10 +19,21 @@ public class PaintController {
     private MenuButton fillMenu;
 
     @FXML
+    private MenuItem line;
+
+    @FXML
+    private MenuItem rectangle;
+
+    @FXML
     private MenuButton typeButton;
 
     @FXML
     private Button undoPressed;
+
+    @FXML
+    void circleChosen(ActionEvent event) {
+
+    }
 
     @FXML
     void clearPressed(ActionEvent event) {
@@ -31,12 +46,22 @@ public class PaintController {
     }
 
     @FXML
-    void fillChosen(ActionEvent event) {
+    void emptyPressed(ActionEvent event) {
 
     }
 
     @FXML
-    void typeChosen(ActionEvent event) {
+    void lineAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rectangle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void solidPressed(ActionEvent event) {
 
     }
 
