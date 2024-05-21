46f207ee8c0fa07b7812237060ce667eda6fa70d
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-21 10:21:55 +0300

    modified:   Maman14/QuestionOne/bin/Node.class 	created constructors:   Maman14/QuestionOne/src/Node.java

--- Maman14/QuestionOne/src/Node.java
+++ Maman14/QuestionOne/src/Node.java
@@ -1,3 +1,25 @@
-public class Node {
+public class Node <E>{
+    private E nodeContent;
+    private E next;
+
+    public Node (E nodeContent) {
+        this.nodeContent = nodeContent;
+        this.next = null;       
+        
+    }
+    public Node (E nodeContent, E next) {
+        this.nodeContent = nodeContent;
+        this.next = next;       
+    }
+
+
+    /*
+     * שדה מטיפוס ג
+- שדה המשמש כמצביע לתא הבא ברשימה.
+.null אי ליצירת תא, המקבל את תוכן התא ויוצר תא עם התוכן הרצוי ומצביע  - ב
+אי ליצירת תא, המקבל את תוכן התא ומצביע לתא הבא ויוצר את התא המבוקש.  - ב
+לעדכון והחזרת תוכן התא. get- ו set - מתודות
+לעדכון והחזרת המצביע לתא הבא. get- ו set - מתודות
+     */
 
 }
