2567c89c4f0370135aeec621c4549ec7cac3c8cf
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-23 13:55:19 +0300

    modified:   Maman14/QuestionOne/bin/Person.class 	added constructor:   Maman14/QuestionOne/src/Person.java

--- Maman14/QuestionOne/src/Person.java
+++ Maman14/QuestionOne/src/Person.java
@@ -1,3 +1,12 @@
 public class Person {
+    private String name;
+    private int id;
+    private String date;
+
+    public Person (String name, int id, String date) {
+        this.name = name;
+        this.id = id;
+        this.date = date;
+    }
 
 }
