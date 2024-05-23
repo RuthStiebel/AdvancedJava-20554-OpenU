bb603c1581aa6b6935ddc22c88b503ee2e0d6a88
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-23 13:59:39 +0300

    added getters:   Maman14/QuestionOne/src/Person.java

--- Maman14/QuestionOne/src/Person.java
+++ Maman14/QuestionOne/src/Person.java
@@ -3,10 +3,23 @@ public class Person {
     private int id;
     private int yearOfBirth;
 
-    public Person (String name, int id, int yearOfBirth) {
+    public Person(String name, int id, int yearOfBirth) {
         this.name = name;
         this.id = id;
         this.yearOfBirth = yearOfBirth;
     }
 
+    // getters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
 }
