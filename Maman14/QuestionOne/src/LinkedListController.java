edf76ab9aba4aac103d11b37300b0da37a48d0ea
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-25 18:28:36 +0300

    new file:   Maman14/QuestionOne/bin/LinkedListController.class 	new file:   Maman14/QuestionOne/bin/LinkedListTester.fxml 	new file:   Maman14/QuestionOne/src/LinkedListController.java

---
+++ Maman14/QuestionOne/src/LinkedListController.java
@@ -0,0 +1,35 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LinkedListController {
+
+    @FXML
+    private TextField fiveString;
+
+    @FXML
+    private TextField fourString;
+
+    @FXML
+    private TextField oneString;
+
+    @FXML
+    private TextField sixString;
+
+    @FXML
+    private TextField threeString;
+
+    @FXML
+    private TextField twoString;
+
+    @FXML
+    void cancelPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void okPressed(ActionEvent event) {
+
+    }
+
+}
