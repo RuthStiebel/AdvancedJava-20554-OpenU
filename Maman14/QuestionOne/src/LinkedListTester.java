3c162bd28b7270451483183bf8a1c70f4473ac29
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-26 13:11:45 +0300

    modified:   Maman14/QuestionOne/bin/LinkedListTester.class 	modified:   Maman14/QuestionOne/bin/LinkedListTester.fxml 	modified:   Maman14/QuestionOne/src/LinkedListTester.java

--- Maman14/QuestionOne/src/LinkedListTester.java
+++ Maman14/QuestionOne/src/LinkedListTester.java
@@ -1,11 +1,37 @@
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import java.util.Scanner;
 
-public class LinkedListTester {
+public class LinkedListTester extends Application{
     private static int MAX = 6;
     
+        /**
+     * The main method to launch the application.
+     */
     public static void main(String[] args) {
+        launch(args);
+        System.out.println();
+    }
+
+    /**
+     * Starts the JavaFX application by loading the FXML file and setting up the
+     * stage.
+     * 
+     * @param stage The primary stage of the application.
+     * @throws Exception If an error occurs while loading the FXML file.
+     */
+    public void start(Stage stage) throws Exception {
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("LinkedListTester.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("LinkedListTester");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void runApp() {
         Scanner scan = new Scanner(System.in);
         String[] list = new String[MAX];
 
