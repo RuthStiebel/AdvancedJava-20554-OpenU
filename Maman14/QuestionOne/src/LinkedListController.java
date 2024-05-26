397b3f85990bbdbe6139e7c6910fdf390f1d5fed
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-26 14:32:03 +0300

    removed cancel button:   Maman14/QuestionOne/bin/LinkedListTester.fxml 	update according to button removal:   Maman14/QuestionOne/src/LinkedListController.java

--- Maman14/QuestionOne/src/LinkedListController.java
+++ Maman14/QuestionOne/src/LinkedListController.java
@@ -1,8 +1,11 @@
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class LinkedListController {
     private LinkedList<String> myList;
@@ -26,21 +29,16 @@ public class LinkedListController {
     private TextField string6;
 
     @FXML
-    void cancelPressed(ActionEvent event) {
-        // just close the window
-
-    }
-
-    @FXML
     void okPressed(ActionEvent event) {
         myList = new LinkedList<String>();
-        TextField[] textFields = {string1, string2, string3, string4, string5, string6};
+        TextField[] textFields = { string1, string2, string3, string4, string5, string6 };
         boolean showAlert = false; // Flag to track if an alert has been shown
-    
+
         for (int i = 0; i < textFields.length; i++) {
             TextField textField = textFields[i];
             if (textField.getText().trim().isEmpty()) {
-                showAlert("ERROR", "Invalid input", "All fields must contain at least one non-whitespace character.", false);
+                showAlert("ERROR", "Invalid input", "All fields must contain at least one non-whitespace character.",
+                        false);
                 showAlert = true; // Set flag to true if an alert is shown
                 break; // Exit the loop if an alert is shown
             } else {
@@ -48,13 +46,12 @@ public class LinkedListController {
                 myList.addNode(newNode);
             }
         }
-    
+
         // Reset linked list if an alert was shown
         if (showAlert) {
             myList = null;
         }
     }
-    
 
     public static void showAlert(String title, String header, String content, boolean flag) {
         Alert alert = new Alert(AlertType.CONFIRMATION);
