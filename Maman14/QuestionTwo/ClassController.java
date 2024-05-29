c2c190592a63ec4d5100d53f5cecd2a971687284
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-29 15:13:44 +0300

    new file:   Maman14/QuestionTwo/ClassController.java 	new file:   Maman14/QuestionTwo/MainClass.java 	new file:   Maman14/QuestionTwo/lib/MainClass.fxml

---
+++ Maman14/QuestionTwo/ClassController.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ClassController {
+
+    @FXML
+    void SearchPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closePressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removePressed(ActionEvent event) {
+
+    }
+
+}
---
+++ Maman14/QuestionTwo/MainClass.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
+    
+    /**
+     * The main method to launch the application.
+     */
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println();
+    }
+
+    /**
+     * Starts the JavaFX application by loading the FXML file and setting up the
+     * stage.
+     * 
+     * @param stage The primary stage of the application.
+     * @throws Exception If an error occurs while loading the FXML file.
+     */
+    public void start(Stage stage) throws Exception {
+        System.out.println("HEY");
+        /*
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("Logic.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Logic");
+        stage.setScene(scene);
+        stage.show();
+        */
+    }
+
+
+}
