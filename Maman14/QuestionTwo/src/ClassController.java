7e13692c6b5bf9db883e5910dfca13b1f0cbcb54
Ruth Stiebel <stiebelruth@gmail.com>
2024-05-31 10:27:05 +0300

    modified:   Maman14/QuestionTwo/bin/ClassController.class added alert method:   Maman14/QuestionTwo/src/ClassController.java

--- Maman14/QuestionTwo/src/ClassController.java
+++ Maman14/QuestionTwo/src/ClassController.java
@@ -1,5 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class ClassController {
 
@@ -15,12 +17,25 @@ public class ClassController {
 
     @FXML
     void closePressed(ActionEvent event) {
+        // close the window
 
     }
 
     @FXML
     void removePressed(ActionEvent event) {
+        
 
+    }
+
+    public static void showAlert(String title, String header, String content, boolean flag) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        if (!flag) {
+            alert.setAlertType(AlertType.ERROR);
+        }
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
 }
