7432b5e406000d98c04733126d681256e3c89b8a
Ruth Stiebel <stiebelruth@gmail.com>
2024-06-03 12:39:47 +0300

    added API:   Maman14/QuestionTwo/src/ClassController.java

--- Maman14/QuestionTwo/src/ClassController.java
+++ Maman14/QuestionTwo/src/ClassController.java
@@ -4,12 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+
 public class ClassController {
     private Dictionary myDict = new Dictionary();
 
     @FXML
     private TextField text;
 
+    /* Handles the search button. */
     @FXML
     void SearchPressed(ActionEvent event) {
         String str = myDict.searchWord(text.getText());
@@ -20,14 +22,15 @@ public class ClassController {
             showAlert("Error", "Oops!", "Word not found.", false);
     }
 
+    /* Handles the add button. */
     @FXML
     void addPressed(ActionEvent event) {
         TextInputDialog definition = new TextInputDialog();
         definition.setHeaderText("Enter word definition:");
-    
-        // Show the text input dialog and wait for user input
+
+        // Shows the text input dialog and wait for user input
         definition.showAndWait().ifPresent(result -> {
-            // After user input, try to add the word to the dictionary
+            // After user input, tries to add the word to the dictionary
             String str = myDict.addWord(text.getText(), result);
             if (str == null) {
                 showAlert("Success", "Yay!", "Word added successfully.", true);
@@ -38,7 +41,8 @@ public class ClassController {
             }
         });
     }
-    
+
+    /* Handles the remove button. */
     @FXML
     void removePressed(ActionEvent event) {
         String str = myDict.removeWord(text.getText());
@@ -52,6 +56,10 @@ public class ClassController {
 
     }
 
+    /*
+     * Displays an alert dialog with the specified title, header, and content. The
+     * alert type is indicated by the flag (true - confirmation, false - error).
+     */
     public static void showAlert(String title, String header, String content, boolean flag) {
         Alert alert = new Alert(AlertType.CONFIRMATION);
         if (!flag) {
