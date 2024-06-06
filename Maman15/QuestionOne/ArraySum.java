1465c35bcef18fc7fd849228c2e8b24b560402b7
Ruth Stiebel <stiebelruth@gmail.com>
2024-06-06 10:06:19 +0300

    added sum methods:   Maman15/QuestionOne/ArraySum.java

--- Maman15/QuestionOne/ArraySum.java
+++ Maman15/QuestionOne/ArraySum.java
@@ -1,30 +1,43 @@
-import java.util.LinkedList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ArraySum {
     private Lock[] locks;
-    private int[] arrNum;
-    private LinkedList<Integer> listSum;
+    private int[] givenArr;
+    private int[] arrSum;
 
-    public ArraySum (int[] arr) {
-        listSum = new LinkedList<Integer>();
-        arrNum = arr;
+    public ArraySum(int[] arr) {
+        givenArr = arr;
+        arrSum = new int[givenArr.length / 2];
         locks = new Lock[arr.length];
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             locks[i] = new ReentrantLock();
         }
     }
 
+    public int getSum(int[] arr) {
+        if (arr.length == 1)
+            return arrSum[0];
+        return getSum(addArray());
+    }
 
+    private int[] addArray() {
+        int[] arr = new int[arrSum.length / 2];
+        int i = 0;
+        while (i < givenArr.length - 2) {
+            arr[i] = sumOfTwo(i, i + i);
+            i += 2;
+        }
+        arrSum = arr;
+        return arrSum;
+    }
 
     private int sumOfTwo(int i, int j) {
         locks[i].lock();
         locks[j].lock();
         try {
-            return arrNum[i] + arrNum[j];
-        }
-        finally {
+            return givenArr[i] + givenArr[j];
+        } finally {
             locks[i].unlock();
             locks[j].unlock();
         }
