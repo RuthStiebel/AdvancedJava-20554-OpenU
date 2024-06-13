8cb58a2804bedac93fba95d106e589ad5f95abed
Ruth Stiebel <stiebelruth@gmail.com>
2024-06-13 23:43:57 +0300

    modified:   Maman15/QuestionOne/ArraySumTester.java

--- Maman15/QuestionOne/ArraySumTester.java
+++ Maman15/QuestionOne/ArraySumTester.java
@@ -1,18 +1,18 @@
 public class ArraySumTester {
     public static void main(String[] args) {
-        // Create an array to sum
-        int[] array = {1, 2, 3, 4, 5, 6};
-
-        // Define the number of concurrent threads
+        // Create an array of integers
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        // Number of threads to use for parallel processing
         int numThreads = 4;
-
-        // Create an instance of ArraySum
-        ArraySum arraySum = new ArraySum(array,// numThreads);
-
+        
+        // Create an instance of ArraySum with the array and number of threads
+        ArraySum arraySum = new ArraySum(arr, numThreads);
+        
         // Calculate the sum of the array
         int sum = arraySum.getSum();
-
+        
         // Print the result
-        System.out.println("Sum of the array: " + sum);
+        System.out.println("The sum of the array is: " + sum);
     }
 }
